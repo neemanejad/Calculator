b87417597969e01373efd5de98ce856a4411f56c
Neema Nejad <dnnejad@gmail.com>
2019-03-07 14:30:44 -0800

    The user is now able to use the muber pad on the keyboard on number keys at the top of their keyboard to input the numbers and operations they'd like to use. Also added exceptions when the user tries to add and the result text is a "cannot divide by 0" message or "overflow" message.

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -245,44 +245,77 @@ namespace Calculator
 
         private void plusClick(object sender, RoutedEventArgs e)
         {
-
-            CheckOperators(); //Checks if user selected a different operator before and unsets it
-            calculator.SetFirstNumber(long.Parse(resultBox.Text));
-            calculator.setPlus();
-            resultBox.Text = "0";
-            
+            try
+            {
+                CheckOperators(); //Checks if user selected a different operator before and unsets it
+                calculator.SetFirstNumber(long.Parse(resultBox.Text));
+                calculator.setPlus();
+                resultBox.Text = "0";
+            } catch (FormatException)
+            {
+                resultBox.Text = resultBox.Text;
+            }
         }
 
         private void minusClick(object sender, RoutedEventArgs e)
         {
-            CheckOperators(); //Checks if user selected a different operator before and unsets it
-            calculator.SetFirstNumber(long.Parse(resultBox.Text));
-            calculator.setMinus();
-            resultBox.Text = "0";
+            try
+            {
+                CheckOperators(); //Checks if user selected a different operator before and unsets it
+                calculator.SetFirstNumber(long.Parse(resultBox.Text));
+                calculator.setMinus();
+                resultBox.Text = "0";
+            } catch (FormatException)
+            {
+                resultBox.Text = resultBox.Text;
+            }
         }
 
         private void multiplyClick(object sender, RoutedEventArgs e)
         {
-            CheckOperators(); //Checks if user selected a different operator before and unsets it
-            calculator.SetFirstNumber(long.Parse(resultBox.Text));
-            calculator.setMult();
-            resultBox.Text = "0";
+            try
+            {
+                CheckOperators(); //Checks if user selected a different operator before and unsets it
+                calculator.SetFirstNumber(long.Parse(resultBox.Text));
+                calculator.setMult();
+                resultBox.Text = "0";
+            } catch (FormatException)
+            {
+                resultBox.Text = resultBox.Text;
+            }
+            
         }
 
         private void divideClick(object sender, RoutedEventArgs e)
         {
-            CheckOperators(); //Checks if user selected a different operator before and unsets it
-            calculator.SetFirstNumber(long.Parse(resultBox.Text));
-            calculator.setDiv();
-            resultBox.Text = "0";
+            try
+            {
+                CheckOperators(); //Checks if user selected a different operator before and unsets it
+                calculator.SetFirstNumber(long.Parse(resultBox.Text));
+                calculator.setDiv();
+                resultBox.Text = "0";
+            }
+            catch (FormatException)
+            {
+                resultBox.Text = resultBox.Text;
+            }
+            
         }
 
         private void exponentClick(object sender, RoutedEventArgs e)
         {
-            CheckOperators(); //Checks if user selected a different operator before and unsets it
-            calculator.SetFirstNumber(long.Parse(resultBox.Text));
-            calculator.setExp();
-            resultBox.Text = "0";
+            try
+            {
+                CheckOperators(); //Checks if user selected a different operator before and unsets it
+                calculator.SetFirstNumber(long.Parse(resultBox.Text));
+                calculator.setExp();
+                resultBox.Text = "0";
+            }
+            catch (FormatException)
+            {
+                resultBox.Text = resultBox.Text;
+            }
+            
         }
 
         private void equalClick(object sender, RoutedEventArgs e)
@@ -295,6 +328,7 @@ namespace Calculator
                     long finalNum = calculator.Add();
                     calculator.setIsThereAnswer();
                     resultBox.Text = finalNum.ToString();
+                    calculator.SetFirstNumber(finalNum);
                 }
                 else if (calculator.Check(calculator.getIsMinus()))
                 {
@@ -302,6 +336,7 @@ namespace Calculator
                     long finalNum = calculator.Subtract();
                     calculator.setIsThereAnswer();
                     resultBox.Text = finalNum.ToString();
+                    calculator.SetFirstNumber(finalNum);
                 }
                 else if (calculator.Check(calculator.getIsMult()))
                 {
@@ -309,6 +344,7 @@ namespace Calculator
                     long finalNum = calculator.Multiply();
                     calculator.setIsThereAnswer();
                     resultBox.Text = finalNum.ToString();
+                    calculator.SetFirstNumber(finalNum);
                 }
                 else if (calculator.Check(calculator.getIsDiv()))
                 {
@@ -316,6 +352,7 @@ namespace Calculator
                     long finalNum = calculator.Divide();
                     calculator.setIsThereAnswer();
                     resultBox.Text = finalNum.ToString();
+                    calculator.SetFirstNumber(finalNum);
                 }
                 else if (calculator.Check(calculator.getIsExp()))
                 {
@@ -323,10 +360,19 @@ namespace Calculator
                     long finalNum = calculator.Exponentiate();
                     calculator.setIsThereAnswer();
                     resultBox.Text = finalNum.ToString();
+                    calculator.SetFirstNumber(finalNum);
                 }
             } catch (DivideByZeroException)
             {
                 resultBox.Text = "Cannot Divide by Zero";
+                calculator.setIsThereAnswer();
+            } catch (OverflowException)
+            {
+                resultBox.Text = "Overflow";
+                calculator.setIsThereAnswer();
+            } catch (FormatException)
+            {
+                resultBox.Text = resultBox.Text;
                 calculator.setIsThereAnswer();
             }
         }
@@ -355,6 +401,68 @@ namespace Calculator
             }
         }
 
-        
+        private void keyboardNumPress(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.NumPad0 || e.Key == Key.D0)
+            {
+                num0Click(sender, e);
+            }
+            else if (e.Key == Key.NumPad1 || e.Key == Key.D1)
+            {
+                num1Click(sender, e);
+            }
+            else if (e.Key == Key.NumPad2 || e.Key == Key.D2)
+            {
+                num2Click(sender, e);
+            }
+            else if (e.Key == Key.NumPad3 || e.Key == Key.D3)
+            {
+                num3Click(sender, e);
+            }
+            else if (e.Key == Key.NumPad4 || e.Key == Key.D4)
+            {
+                num4Click(sender, e);
+            }
+            else if (e.Key == Key.NumPad5 || e.Key == Key.D5)
+            {
+                num5Click(sender, e);
+            }
+            else if (e.Key == Key.NumPad6 || e.Key == Key.D6)
+            {
+                num6Click(sender, e);
+            }
+            else if (e.Key == Key.NumPad7 || e.Key == Key.D7)
+            {
+                num7Click(sender, e);
+            }
+            else if (e.Key == Key.NumPad8 || e.Key == Key.D8)
+            {
+                num8Click(sender, e);
+            }
+            else if (e.Key == Key.NumPad9 || e.Key == Key.D9)
+            {
+                num9Click(sender, e);
+            }
+            else if (e.Key == Key.Add)
+            {
+                plusClick(sender, e);
+            } else if (e.Key == Key.Subtract)
+            {
+                minusClick(sender, e);
+            }
+            else if (e.Key == Key.Multiply)
+            {
+                multiplyClick(sender, e);
+            }
+            else if (e.Key == Key.Divide)
+            {
+                divideClick(sender, e);
+            }
+            else if (e.Key == Key.Enter)
+            {
+                equalClick(sender, e);
+            }
+
+        }
     }
 }
