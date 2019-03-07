12a74975db78f103b9733c3d241b655e44a753fb
Neema Nejad <dnnejad@gmail.com>
2019-03-07 10:03:20 -0800

    Added a "Calc" class to utilize mathematical operations and capture the functionality of the calculator.

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -29,5 +29,56 @@ namespace Calculator
         {
 
         }
+
+        class Calc
+        {
+            private int firstNumber, secondNumber;
+
+
+            Calc(int firstNumber, int secondNumber)
+            {
+                this.firstNumber = firstNumber;
+                this.secondNumber = secondNumber;
+            }
+
+            public static int Add(int firstNumber, int secondNumber)
+            {
+                return firstNumber + secondNumber;
+            }
+
+            public static int Subtract(int firstNumber, int secondNumber)
+            {
+                return firstNumber - secondNumber;
+            }
+
+            public static int Multiply(int firstNumber, int secondNumber)
+            {
+                return firstNumber * secondNumber;
+            }
+
+            public static int Divide(int firstNumber, int secondNumber)
+            {
+                return firstNumber / secondNumber;
+            }
+
+            public static int Exponentiate(int number, int exponent)
+            {
+                for (int i = 1; i < exponent; i++)
+                {
+                    number *= number;
+                }
+
+                return number;
+            }
+
+            public void Clear()
+            {
+                firstNumber = 0;
+                secondNumber = 0;
+            }
+
+
+
+        }
     }
 }
